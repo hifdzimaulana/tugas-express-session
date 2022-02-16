50db80294ff7704c8085e4c0c40cf9fd895cef3c
Hifdzi Maulana <hifdzimaulana12@gmail.com>
2022-02-16 19:37:00 +0700

    indexing modules with glob

--- handlers/index.js
+++ handlers/index.js
@@ -1,3 +1,21 @@
-exports.login = require('./login')
-exports.logout = require('./logout')
-exports.register = require('./register')
+const glob = require('glob')
+const path = require('path')
+
+var options = {
+    cwd: './handlers', // relative to root dir
+    ignore: [
+        path.basename(__filename) // ignoring this file (index.js)
+    ]
+}
+
+// glob.sync means the process will be done sychronously
+glob.sync('*.js', options).forEach(file => {
+    module.exports[path.basename(file, '.js')] = require('./' + file)
+})
+
+// The output will be the same as these
+
+/* exports.login = require('./login')
+* exports.logout = require('./logout)
+* exports.register = require('./register)
+*/
